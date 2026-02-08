b0e9097c76a747fb895f232248c5776a2403c025
MONU KUMAR YADAV <monukumaryadav4128@gmail.com>
2026-02-08 00:21:06 +0000

    dataTypes

---
+++ .devcontainer/Basic/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strinct";//treal all JS code as newer version
+
+
+//alert("3+3")// we are using node js , not browser
+
+console.log(3+
+    3)// code readability should be high
+ console.log("Monu")
+
+ //=================================
+
+ let name="Monu";
+ let age=21;
+ let isLoggedIn=false;
+
+
+ //number==> 2 to the power 53
+ //bigint
+ //string==>""
+ //boolean==>true/false
+ //null==>stand alone value
+ //undefined
+ //symbol ==>unoque
+
+ //object
+
+ console.log(typeof "Monu");
+ console.log(typeof age);
+ console.log(typeof null);
+ console.log(typeof undefined);
+ 
+ 
+ 
+ 
