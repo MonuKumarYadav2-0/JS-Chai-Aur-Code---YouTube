50497b858f7eddc77c90c2ca2653a811f198eb04
MONU KUMAR YADAV <monukumaryadav4128@gmail.com>
2026-02-08 01:03:30 +0000

    conversion and operation completed

--- .devcontainer/Basic/03_conversionOperation.js
+++ .devcontainer/Basic/03_conversionOperation.js
@@ -18,8 +18,47 @@
 //""==>false
 //"Monu"==true
 
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//******************* Operation ************
+
+let value=3
+let negValue=-value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="Hello"
+let str2=" Monu"
+let str3=str1+str2
+
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(2+"1");
+// console.log("1"+2+2);
+// console.log(1+2+"3");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
+
+
+
 
