250fcc3ff3fc47953e3f2799eb7b04b8d4c69ad5
MONU KUMAR YADAV <monukumaryadav4128@gmail.com>
2026-02-11 02:55:37 +0000

    Strings

---
+++ .devcontainer/Basic/05_string.js
@@ -0,0 +1,32 @@
+// const name="Monu"
+// const repoCount=50
+ const myname=new String('Monu Kumar yadav')
+// //console.log(name+repoCount + " value");
+
+// console.log(`hello my name s ${name} and my repo count is ${repoCount}`)
+
+// console.log(myname.length);
+
+// console.log(myname.toUpperCase);
+// console.log(myname);
+
+// const newString=myname.substring(0,4)
+// console.log(newString);
+
+// const anotherString=myname.slice(0,5)
+// console.log(anotherString);
+
+// const newStringOne="        monu    kumar      y"
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url="https://monukumar.com/monu%20kumar"
+// console.log(url);
+
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('monu'));
+
+console.log(myname.split(" "));
+
+
